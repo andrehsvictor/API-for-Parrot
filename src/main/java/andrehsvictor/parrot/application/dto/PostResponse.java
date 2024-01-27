c3def6c237a5faf0dda7aaa369b4c756e0970994
André Victor <andrehsvictor@gmail.com>
2024-01-27 20:58:11 -0300

    Add PostResponse DTO class

---
+++ src/main/java/andrehsvictor/parrot/application/dto/PostResponse.java
@@ -0,0 +1,55 @@
+package andrehsvictor.parrot.application.dto;
+
+import java.time.LocalDateTime;
+
+public record PostResponse(Long id, String title, String content, PostAuthorDetails author, LocalDateTime createdAt) {
+
+    public static PostResponseBuilder builder() {
+        return new PostResponseBuilder();
+    }
+
+    public static class PostResponseBuilder {
+        private Long id;
+        private String title;
+        private String content;
+        private PostAuthorDetails author;
+        private LocalDateTime createdAt;
+
+        PostResponseBuilder() {
+        }
+
+        public PostResponseBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public PostResponseBuilder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public PostResponseBuilder content(String content) {
+            this.content = content;
+            return this;
+        }
+
+        public PostResponseBuilder author(PostAuthorDetails author) {
+            this.author = author;
+            return this;
+        }
+
+        public PostResponseBuilder createdAt(LocalDateTime createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public PostResponse build() {
+            return new PostResponse(id, title, content, author, createdAt);
+        }
+
+        public String toString() {
+            return "PostResponse.PostResponseBuilder(id=" + this.id + ", title=" + this.title + ", content=" + this.content + ", author=" + this.author + ", createdAt=" + this.createdAt + ")";
+        }
+    }
+
+}
