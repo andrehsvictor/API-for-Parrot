f42c08fe2d8444ba49d2912c7fab47d1a37a88d4
André Victor <andrehsvictor@gmail.com>
2024-01-27 20:58:22 -0300

    Add PostGateway interface

---
+++ src/main/java/andrehsvictor/parrot/application/gateway/PostGateway.java
@@ -0,0 +1,13 @@
+package andrehsvictor.parrot.application.gateway;
+
+import java.util.Collection;
+
+import andrehsvictor.parrot.domain.post.Post;
+
+public interface PostGateway {
+    Post save(Post post);
+    void delete(Long id);
+    Post findById(Long id);
+    Post update(Long id, Post post);
+    Collection<Post> findAllOrderByCreatedAtDesc();
+}
