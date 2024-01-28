71c6a7bceb5322faa70381447ce5fb71503445ee
André Victor <andrehsvictor@gmail.com>
2024-01-28 16:18:39 -0300

    Update column names in tables

--- src/main/resources/db/migration/V1__Set_up_tables.sql
+++ src/main/resources/db/migration/V1__Set_up_tables.sql
@@ -9,17 +9,17 @@ create table if not exists posts (
     id serial primary key,
     title varchar(255) not null,
     content text not null,
-    user_id int not null,
+    author_id int not null,
     created_at timestamp default current_timestamp,
-    foreign key (user_id) references users(id)
+    foreign key (author_id) references users(id)
 );
 
 create table if not exists comments (
     id serial primary key,
     content text not null,
-    user_id int not null,
+    author_id int not null,
     post_id int not null,
     created_at timestamp default current_timestamp,
-    foreign key (user_id) references users(id),
+    foreign key (author_id) references users(id),
     foreign key (post_id) references posts(id)
 );
