b536eb426051b1331f93e9eafa2c2f881bba3c94
André Victor <andrehsvictor@gmail.com>
2024-01-28 16:18:46 -0300

    Add user data to users table

---
+++ src/main/resources/db/migration/V2__Populate_table_`users`.sql
@@ -0,0 +1,18 @@
+insert into
+    users (name, password, email)
+values
+    (
+        'andrezao',
+        '$2a$12$ZpED11I1fXOoqj4AKjiCD.gikXh09B3NPK4T7kLrmfkpc2UJoRlSa',
+        'andrezao2004@gmail.com'
+    ),
+    (
+        'andrezao2',
+        '$2a$12$s/5q0LB01vRtbXDwlWKRXukYmhjKPrkYn1CEaow1L4TwiG3kKAfGW',
+        'andrezao2_2004@hotmail.com'
+    ),
+    (
+        'andrezao3',
+        '$2a$12$B6aLagmESBgjJmujWk0RV.JQmlZ40xLkgCIAYzrJO0m8fpW4e.01e',
+        'andrezao3_2004@outlook.com'
+    );
