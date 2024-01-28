01fd0209e3c7b815c5b1d9cc9c13a2914a3bcdeb
André Victor <andrehsvictor@gmail.com>
2024-01-28 15:21:11 -0300

    Add AuthControllerV1_0 with signin, signup, and refresh endpoints

---
+++ src/main/java/andrehsvictor/parrot/infrastructure/controller/AuthControllerV1_0.java
@@ -0,0 +1,36 @@
+package andrehsvictor.parrot.infrastructure.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import andrehsvictor.parrot.application.dto.SigninRequest;
+import andrehsvictor.parrot.application.dto.SignupRequest;
+import andrehsvictor.parrot.application.use_case.AuthService;
+
+@RestController
+@RequestMapping("/api/1.0/auth")
+public class AuthControllerV1_0 {
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> signin(@RequestBody SigninRequest signinRequest) {
+        return ResponseEntity.ok(authService.signin(signinRequest));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody SignupRequest signupRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.signup(signupRequest));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<?> refresh(@RequestHeader(name = "Refresh-Token", required = true) String refreshToken) {
+        return ResponseEntity.ok(authService.refresh(refreshToken));
+    }
+}
