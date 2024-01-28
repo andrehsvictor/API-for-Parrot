86d1b0cb47e83cc3ace5513be99584e688af8b98
André Victor <andrehsvictor@gmail.com>
2024-01-28 20:57:15 -0300

    Add initial comments to the comments table

---
+++ src/main/resources/db/migration/V4__Populate_table_`comments`.sql
@@ -0,0 +1,9 @@
+insert into
+    comments (content, author_id, post_id)
+values
+    ('I like this post!', 1, 1),
+    ('I like this post too!', 2, 1),
+    ('Just commenting', 3, 2),
+    ('Just commenting too', 2, 2),
+    ('Im commenting this post', 1, 3),
+    ('Im commenting this post too', 2, 3);
