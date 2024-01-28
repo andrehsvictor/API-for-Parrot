029b80fcf469b783c8242e119ca29e81c2abc5cc
André Victor <andrehsvictor@gmail.com>
2024-01-28 20:57:34 -0300

    Add comments field to PostResponse DTO

--- src/main/java/andrehsvictor/parrot/application/dto/PostResponse.java
+++ src/main/java/andrehsvictor/parrot/application/dto/PostResponse.java
@@ -1,8 +1,9 @@
 package andrehsvictor.parrot.application.dto;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 
-public record PostResponse(Long id, String title, String content, AuthorDetails author, LocalDateTime createdAt) {
+public record PostResponse(Long id, String title, String content, AuthorDetails author, Collection<PostCommentDetails> comments, String createdAt) {
 
     public static PostResponseBuilder builder() {
         return new PostResponseBuilder();
@@ -13,7 +14,8 @@ public record PostResponse(Long id, String title, String content, AuthorDetails 
         private String title;
         private String content;
         private AuthorDetails author;
-        private LocalDateTime createdAt;
+        private Collection<PostCommentDetails> comments;
+        private String createdAt;
 
         PostResponseBuilder() {
         }
@@ -38,17 +40,22 @@ public record PostResponse(Long id, String title, String content, AuthorDetails 
             return this;
         }
 
-        public PostResponseBuilder createdAt(LocalDateTime createdAt) {
+        public PostResponseBuilder comments(Collection<PostCommentDetails> comments) {
+            this.comments = comments;
+            return this;
+        }
+
+        public PostResponseBuilder createdAt(String createdAt) {
             this.createdAt = createdAt;
             return this;
         }
 
         public PostResponse build() {
-            return new PostResponse(id, title, content, author, createdAt);
+            return new PostResponse(id, title, content, author, comments, createdAt);
         }
 
         public String toString() {
-            return "PostResponse.PostResponseBuilder(id=" + this.id + ", title=" + this.title + ", content=" + this.content + ", author=" + this.author + ", createdAt=" + this.createdAt + ")";
+            return "PostResponse.PostResponseBuilder(id=" + this.id + ", title=" + this.title + ", content=" + this.content + ", author=" + this.author + ", comments=" + this.comments + ", createdAt=" + this.createdAt + ")";
         }
     }
 
