31c9374a1fc9f13d561945d4bbcd3734e7120b44
André Victor <andrehsvictor@gmail.com>
2024-01-28 15:21:04 -0300

    Add BeansConfig.java

---
+++ src/main/java/andrehsvictor/parrot/infrastructure/config/BeansConfig.java
@@ -0,0 +1,30 @@
+package andrehsvictor.parrot.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import andrehsvictor.parrot.application.gateway.AuthenticatedUserProvider;
+import andrehsvictor.parrot.application.gateway.PostGateway;
+import andrehsvictor.parrot.application.use_case.PostService;
+import andrehsvictor.parrot.infrastructure.persistence.post.PostGatewayImpl;
+import andrehsvictor.parrot.infrastructure.util.AuthenticatedUserProviderImpl;
+
+@Configuration
+public class BeansConfig {
+
+    @Bean
+    PostService postService() {
+        return new PostService(postGateway(), authenticatedUserProvider());
+    }
+
+    @Bean
+    PostGateway postGateway() {
+        return new PostGatewayImpl();
+    }
+
+    @Bean
+    AuthenticatedUserProvider authenticatedUserProvider() {
+        return new AuthenticatedUserProviderImpl();
+    }
+    
+}
