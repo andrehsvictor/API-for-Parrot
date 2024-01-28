9571d519f8aa2878275997b83e46908b63e6dba7
André Victor <andrehsvictor@gmail.com>
2024-01-28 20:57:02 -0300

    Add initial data to posts table

---
+++ src/main/resources/db/migration/V3__Populate_table_`posts`.sql
@@ -0,0 +1,7 @@
+insert into
+    posts (title, content, author_id)
+values
+    ('Hello', 'I m the first post in this social network :)', 1),
+    ('Hi', 'Just a random post :v', 2),
+    ('Welcome', 'Welcome to this social network!', 3),
+    ('Just posting', 'Im liking it kkkk', 3);
