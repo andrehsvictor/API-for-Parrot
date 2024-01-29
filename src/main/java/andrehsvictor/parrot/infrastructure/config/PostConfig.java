cf1a58765287b841748114db407d31d4dccb9e2e
André Victor <andrehsvictor@gmail.com>
2024-01-29 10:01:34 -0300

    Refactor PostConfig to use constructor injection

--- src/main/java/andrehsvictor/parrot/infrastructure/config/PostConfig.java
+++ src/main/java/andrehsvictor/parrot/infrastructure/config/PostConfig.java
@@ -7,28 +7,21 @@ import andrehsvictor.parrot.application.gateway.AuthenticatedUserProvider;
 import andrehsvictor.parrot.application.gateway.PostGateway;
 import andrehsvictor.parrot.application.usecase.post.CreatePostUseCase;
 import andrehsvictor.parrot.application.usecase.post.DeletePostUseCase;
-import andrehsvictor.parrot.application.usecase.post.FindAllPostsUseCase;
 import andrehsvictor.parrot.application.usecase.post.FindPostUseCase;
 import andrehsvictor.parrot.application.usecase.post.UpdatePostUseCase;
 import andrehsvictor.parrot.infrastructure.persistence.post.PostGatewayImpl;
-import andrehsvictor.parrot.infrastructure.util.AuthenticatedUserProviderImpl;
 
 @Configuration
 public class PostConfig {
 
     @Bean
-    CreatePostUseCase createPostUseCase() {
-        return new CreatePostUseCase(postGateway(), authenticatedUserProvider());
+    CreatePostUseCase createPostUseCase(AuthenticatedUserProvider authenticatedUserProvider) {
+        return new CreatePostUseCase(postGateway(), authenticatedUserProvider);
     }
 
     @Bean
-    FindAllPostsUseCase findAllPostsUseCase() {
-        return new FindAllPostsUseCase(postGateway());
-    }
-
-    @Bean
-    DeletePostUseCase deletePostUseCase() {
-        return new DeletePostUseCase(postGateway(), authenticatedUserProvider());
+    DeletePostUseCase deletePostUseCase(AuthenticatedUserProvider authenticatedUserProvider) {
+        return new DeletePostUseCase(postGateway(), authenticatedUserProvider);
     }
 
     @Bean
@@ -37,17 +30,12 @@ public class PostConfig {
     }
 
     @Bean
-    UpdatePostUseCase updatePostUseCase() {
-        return new UpdatePostUseCase(postGateway(), authenticatedUserProvider());
+    UpdatePostUseCase updatePostUseCase(AuthenticatedUserProvider authenticatedUserProvider) {
+        return new UpdatePostUseCase(postGateway(), authenticatedUserProvider);
     }
 
     @Bean
     PostGateway postGateway() {
         return new PostGatewayImpl();
-    }
-
-    @Bean
-    AuthenticatedUserProvider authenticatedUserProvider() {
-        return new AuthenticatedUserProviderImpl();
     }
 }
